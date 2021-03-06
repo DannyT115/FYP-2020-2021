c900ccb57a70a6973448798947e2e96cf0311dda
Daniel Taylor <44467609+DannyT115@users.noreply.github.com>
2021-03-06 22:24:39 +0000

    Added ScoreCounter
    [ADDED] ScoreBoard for hoops scored.
    [ADDED] Script to change scoreboard depending on hoops scored.
    [MODIFIED] Ball spawn location.
    [MODIFIED] Hoop trigger collider (harder to hit but more accurate / realistic).

---
+++ VR_Simulation/Assets/CountHoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
+ using TMPro;
+
+public class CountHoop : MonoBehaviour
+{
+public TextMeshPro PlayerScoreText;
+public int scoreValue;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreValue = 0;
+
+    }
+    void OnTriggerEnter()
+    {
+        scoreValue = scoreValue + 1; //add 1 to the score
+        PlayerScoreText.text = scoreValue.ToString();
+        //AudioSource.PlayClipAtPoint(scoreSoundn); //TODO: play score sound
+    }
+}
