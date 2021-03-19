d84db5b7ad21a826c0ed6b9dcd945417f0d89f30
Daniel Taylor <44467609+DannyT115@users.noreply.github.com>
2021-03-19 03:06:20 +0000

    Created respawnBall script
    [ADDED] respawnBall.cs
    - The respawn ball script detects each side of the fence (north, east, south, west), as a trigger. When a ball goes through a trigger it respawns.

--- VR_Simulation/Assets/Project/Scripts/respawnBall.cs
+++ VR_Simulation/Assets/Project/Scripts/respawnBall.cs
@@ -6,16 +6,45 @@ using System.Threading;
 public class respawnBall : MonoBehaviour
 {
     Vector3 ballSpawnPoint;
+    Rigidbody rb;
+
+    public List<GameObject> Boundaries = new List<GameObject>();
 
     void Start() {
+
+        rb = GetComponent<Rigidbody>();
+
         ballSpawnPoint = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        //Find each "Boundary" trigger (located at the fence on each side),
+        //then add each Boundary trigger to list
+        GameObject NB = GameObject.Find("NorthBoundary");
+        Boundaries.Add(NB);
+        GameObject EB = GameObject.Find("EastBoundary");
+        Boundaries.Add(EB);
+        GameObject SB = GameObject.Find("SouthBoundary");
+        Boundaries.Add(SB);
+        GameObject WB = GameObject.Find("WestBoundary");
+        Boundaries.Add(WB);
     }
  
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.name == "Floor") {
-        Thread.Sleep(3000);
-        transform.position =  ballSpawnPoint;
+        //Loop through each trigger in list, 
+        //if collision is detected call RespawnBall function
+        foreach (GameObject bd in Boundaries) {
+            if (col.gameObject == bd ) {
+            Invoke("RespawnBall", 2);
+            }
         }
+    }
+
+    //RespawnBall, moves it back to original position,
+    //whilst simultaneously setting its velocity to 0.
+    void RespawnBall()
+    {
+        transform.position =  ballSpawnPoint;
+        rb.velocity = Vector3.zero; //Slowing down the ball
+        rb.angularVelocity = Vector3.zero;
     }
 }
